eb3409360ccc489bf3bc83ba86abfcdae85ee86a
Nicola Mosco <nicola.mosco@gmail.com>
2024-12-08 11:03:41 +0100

    day 06 2024

--- events/2024/day-06/guard_gallivant.jl
+++ events/2024/day-06/guard_gallivant.jl
@@ -1,6 +1,6 @@
 #%%
 cd(@__DIR__)
-input = stack(eachline("input.txt"))
+patrolmap = stack(eachline("input.txt"))
 
 #%%
 function moveguard(patrolmap)
@@ -28,3 +28,39 @@ function moveguard(patrolmap)
 end
 
 visited, patrolmap = moveguard(input)
+
+
+#%%
+function moveguard2(patrolmap)
+    curpos = findfirst(==('^'), patrolmap)
+    isnothing(curpos) && error("starting position not found")
+    direction = CartesianIndex(0,-1)
+    visited = Set{Tuple{CartesianIndex{2},CartesianIndex{2}}}([(curpos, direction)])
+    isloop = false
+    while true
+        nextpos = curpos + direction
+        i, j = nextpos.I
+        if i ∉ axes(patrolmap, 1) || j ∉ axes(patrolmap, 2)
+            break
+        elseif patrolmap[nextpos] == '#' || patrolmap[nextpos] == 'O'
+            i, j = direction.I
+            direction = CartesianIndex(-j, i)
+            continue
+        end
+        curpos = nextpos
+        if (curpos, direction) ∈ visited
+            @info "loop detected: $curpos"
+            isloop = true
+            break
+        end
+        push!(visited, (curpos, direction))
+    end
+    visited_pos = map(first, collect(visited))
+    directions = map(last, collect(visited))
+    return visited_pos, isloop
+end
+
+function
+end
+
+visited, patrolmap = moveguard2(patrolmap)
